8317f27210bc5c80ff523ce6d3fa2cdaa9ade00a
kedmead91 <73185203+kedmead91@users.noreply.github.com>
2020-11-08 23:32:40 +0000

    Add files via upload

---
+++ favourite_meal.py
@@ -0,0 +1,34 @@
+# Beginning & Defining of function
+def favourite_meal():
+    # requesting user input
+    user = input("Hello what is your name? ")
+    # prints hello & users name
+    print("Hello " + user)
+    print("Can I ask you questions about your favourite meal?")
+    # asks user if they want to take part
+    yes_no = input("Please press 1 for Yes and 2 for No ")
+    # if 1 then the function goes ahead if 2 then function will end
+    if yes_no == '2':
+        quit("Okay, maybe another time")
+    else:
+        # asks first question (starter)
+        print("Okay thanks, First Question: ")
+        starter = input("What is your favourite starter? ")
+    # asks second question (main course)
+        main_course = input("What is your favourite main course? ")
+    # asks third question (dessert)
+        dessert = input("What is your favourite dessert? ")
+    # asks fourth question (alcoholic drink)
+        drink = input("Finally what is your favourite alcoholic drink? ")
+    # prints the users favourite three course meal
+        print("Your favourite three course meal consists of: ")
+        print("A starter of " + starter + "," + " a main course of " + main_course + " and " + dessert + " for dessert")
+    # then prints the users favourite alcoholic drink
+        print(" and don't forget your favourite drink of " + drink + "!")
+
+
+
+# calling function
+
+
+favourite_meal()
---
+++ palindrome.py
@@ -0,0 +1,9 @@
+def palindrome():
+        string = input("Enter a string:")
+        if string == string[::-1]:
+         print("The string is a palindrome")
+        else:
+         print("Not a palindrome")
+
+
+palindrome()
