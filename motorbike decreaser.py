52ee7e35687136d1074cb0469705c463627f29ba
Kamara <73185203+kedmead91@users.noreply.github.com>
2021-02-07 22:19:06 +0000

    Add files via upload

--- motorbike decreaser.py
+++ motorbike decreaser.py
@@ -1,10 +1,18 @@
+# Program shows 10% decrease in motorbike value depending on how many years user has had it
 while True:
+    # asks user how many years they have had their motorbike
     years = float(input("How many years have you had your motorbike? "))
+    # motobikes starting price
     motorbikeStartPrice = 2000
+    # percentage that will be deducted
     percentageDiscount = float(10)
+    # calculates decreased total
     reducedPrice = motorbikeStartPrice - motorbikeStartPrice * years * percentageDiscount / 100
-    print("In the next " + str(years) + " year/s, your motorbike will be worth " + str(reducedPrice) + " pounds.")
+    # prints how long user has had motorbike and the amount it is worth
+    print("You have had your motorbike " + str(years) + " year/s, it is now worth " + str(reducedPrice) + " pounds.")
+    # asks user if they want to rerun program to check other years totals
     rerun = input("Would you like to run this program again? 1 for Yes or 2 for No. ")
 
     if rerun != "1":
         break
+# loop ends when user inputs 2
